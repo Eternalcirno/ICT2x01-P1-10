c8c434556af70b5625a923991d65ba084d533178
Yuan Shengh Seah <71884484+Eternalcirno@users.noreply.github.com>
2021-11-10 22:34:33 +0800

    login and pass change done?

--- portal.py
+++ portal.py
@@ -1,22 +1,27 @@
 from flask import *
 from markupsafe import Markup
-from decouple import config
 import dotenv
+import time
 import re
+import secrets
+import hashlib
 
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 config = dotenv.dotenv_values(dotenv_file)
 uname = config["UNAME"]
 passwd = config["PASS"]
-print(passwd)
+ratelimit = 0
+btime = 0
+banned = False
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(32)
 
 @app.route("/",methods=["GET"])
 def homepage():
     print()
-    return render_template("./index.html")
+    return render_template("./LOL.html")
 
 @app.route("/admin", methods=["GET"])
 def admin():
@@ -24,8 +29,30 @@ def admin():
 
 @app.route("/auth", methods=["POST"])
 def login():
-    pass
-   # ???
+    now = time.time()
+    global btime
+    global ratelimit
+    global banned
+    if(banned and (now - btime) >= 10):
+        ratelimit = 0
+        banned = False
+    user = request.form.get("login")
+    password = request.form.get("password")
+    hpassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    if(user == ""):
+        return "Username cannot be empty"
+    if(password == ""):
+        return "Password cannot be empty"
+    if(user == uname and hpassword == passwd):
+        flash("Successfully logged in")
+        return render_template("./panel.html")
+    if(user != uname or hpassword != passwd):
+        if(ratelimit >= 5 and banned == False):
+            btime = time.time()
+            banned = True
+            return "5 invalid tries, account locked out"
+        ratelimit += 1
+        return "Invalid username or password"
 
 @app.route("/changespeed", methods=["POST"])
 def cspeed():
@@ -42,19 +69,30 @@ def cpass():
     if(request.method == "GET"):
         return render_template("./changepass.html")
     print(request.form)
+    global passwd
+    a = passwd
     oldpass = request.form.get("oldpass")
+    holdpass = hashlib.sha256(oldpass.encode('utf-8')).hexdigest()
     newpass = request.form.get("newpass")
     cnewpass = request.form.get("cfmnewpass")
-    if(oldpass != passwd):
-        return "incorrect old password"
+    if(oldpass == newpass):
+        return "Old password cannot be the same as new password"
+    if(oldpass == ""):
+        return "Old password cannot be empty"
+    if(newpass == ""):
+        return "New password cannot be empty"
+    if(cnewpass == ""):
+        return "Confirm new password cannot be empty"
+    if(holdpass != a):
+        return "Incorrect old password"
     elif(newpass != cnewpass):
-        return "passwords do not match"
+        return "Passwords do not match"
     elif(not (re.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$", newpass))):
-        return "password should be 6-20 chars long with upper and lower case characters and special characters"
-    elif(oldpass == passwd):
-        dotenv.set_key(dotenv_file, "PASS", newpass)
-        global config 
-        config = dotenv.dotenv_values(dotenv_file)
+        return "Password should be 6-20 chars long with upper and lower case characters and special characters"
+    elif(holdpass == a):
+        hashed = hashlib.sha256(newpass.encode('utf-8')).hexdigest()
+        dotenv.set_key(dotenv_file, "PASS", hashed)
+        passwd = dotenv.dotenv_values(dotenv_file)["PASS"]
         return "password successfully changed"
 
 if __name__ == "__main__":
