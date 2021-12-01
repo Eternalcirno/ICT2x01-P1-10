8f336a1c4a597c307310529ecd44759fa9dc60e1
Lutfee <mynameislutfee@gmail.com>
2021-12-01 14:03:22 +0800

    cleanup and fixes

--- portal.py
+++ portal.py
@@ -175,4 +175,5 @@ def cpass():
         return "password successfully changed"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    #app.run(host='0.0.0.0', port=80)
+    app.run()
