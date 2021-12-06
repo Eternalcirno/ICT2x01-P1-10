4b4497254b7a48cbfc5697258ee0b8fbbc6b8a18
NG Jing Fang <j.coolscorpio@gmail.com>
2021-12-07 01:15:33 +0800

    admin setting with bug

--- portal.py
+++ portal.py
@@ -1,34 +1,19 @@
+import auth
 import hashlib
-import re
 import secrets
-import sqlite3 as sql
-import time
-
+import db
 import dotenv
+import os
 from flask import *
+from werkzeug.utils import secure_filename
 
 ratelimit = 0
 btime = 0
 banned = False
 
-conn = sql.connect('database.db')
-print("Opened database successfully")
 
-conn.execute('CREATE TABLE IF NOT EXISTS speed_table (data TEXT)')
-conn.execute('CREATE TABLE IF NOT EXISTS line_table (data TEXT)')
-conn.execute('CREATE TABLE IF NOT EXISTS distance_table (data TEXT)')
-conn.execute('CREATE TABLE IF NOT EXISTS commands_table (data TEXT)')
-conn.execute('CREATE TABLE IF NOT EXISTS start_robot (data TEXT)')
-conn.execute('CREATE TABLE IF NOT EXISTS checkpoint (data TEXT)')
-with sql.connect("database.db") as con:
-    cur = con.cursor()
-    #     cur.execute('INSERT INTO speed_table(data) VALUES(0)')
-    #     cur.execute('INSERT INTO line_table(data) VALUES(0)')
-    #     cur.execute('INSERT INTO distance_table(data) VALUES(0)')
-    cur.execute('INSERT INTO start_robot(data) VALUES(0)')
-# conn.execute('DROP TABLE data_table')
-# print("Table created successfully")
-conn.close()
+db.reset_database()
+print("Opened database successfully")
 
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
@@ -39,11 +24,11 @@ print(passwd)
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)
+a = auth.Auth(uname, passwd)
 
 
 @app.route("/", methods=["GET"])
 def homepage():
-    print()
     return render_template("./homepage.html")
 
 
@@ -55,60 +40,22 @@ def data():
         distance = request.args.get('distance')
         start_robot = request.args.get('start_robot')
         checkpoint = request.args.get('checkpoint')
-        with sql.connect("database.db") as con:
-            cur = con.cursor()
-            if speed is not None:
-                cur.execute('UPDATE speed_table SET data=? WHERE rowid=1', (speed,))
-            if line is not None:
-                cur.execute('UPDATE line_table SET data=? WHERE rowid=1', (line,))
-            if distance is not None:
-                cur.execute('UPDATE distance_table SET data=? WHERE rowid=1', (distance,))
-            if start_robot is not None:
-                cur.execute('UPDATE start_robot SET data=? WHERE rowid=1', (start_robot,))
-            if checkpoint is not None:
-                cur.execute('UPDATE checkpoint SET data=? WHERE rowid=1', (checkpoint,))
-            con.commit()
+        db.update_data(speed, line, distance, start_robot, checkpoint)
 
-    with sql.connect("database.db") as con:
-        cur = con.cursor()
-        cur.execute("select * from speed_table")
-        row = cur.fetchone()
-        speed = row[0]
-
-        cur.execute("select * from line_table")
-        row = cur.fetchone()
-        line = row[0]
-
-        cur.execute("select * from distance_table")
-        row = cur.fetchone()
-        distance = row[0]
-
-        cur.execute("select * from start_robot")
-        row = cur.fetchone()
-        start_robot = row[0]
-
-        cur.execute("select * from checkpoint")
-        row = cur.fetchone()
-        checkpoint = row[0]
+    speed = db.get_speed()
+    line = db.get_line()
+    distance = db.get_distance()
+    start_robot = db.get_start()
+    checkpoint = db.get_checkpoint()
 
     return render_template("./data.html", speed=speed, line=line, distance=distance, start_robot=start_robot, checkpoint=checkpoint)
 
 
 @app.route("/commands", methods=["GET"])
 def commands():
-    with sql.connect("database.db") as con:
-        cur = con.cursor()
-        cur.execute("select * from commands_table")
-        row = cur.fetchone()
-        command = "[" + row[0] + "]"
-    # return render_template("./commands.html",command=command)
+    command = db.get_commands()
+    print("command", command)
     return command
-
-
-@app.route("/start", methods=["GET"])
-def start():
-    start = "1"
-    return render_template("./start.html", start=start)
 
 
 @app.route("/admin", methods=["GET"])
@@ -124,21 +71,10 @@ def logout():
 
 @app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
-    with sql.connect("database.db") as con:
-        cur = con.cursor()
-        cur.execute("select * from commands_table")
-        row = cur.fetchone()
-
-        cur.execute("select * from checkpoint")
-        row = cur.fetchone()
-        checkpoint = row[0]
-
-
-        command = "[" + row[0] + "]"
-
-        return render_template("./dashboard.html",command=command,checkpoint=checkpoint)
-    #return command
-    #return render_template("./dashboard.html", speed=speed,checkpoint=checkpoint,)
+    command = db.get_commands()
+    line = db.get_line()
+    distance = db.get_distance()
+    return render_template("./dashboard.html",command=command, line=line, distance=distance)
 
 
 @app.route("/layout", methods=["GET", "POST"])
@@ -148,104 +84,140 @@ def layout():
 
 @app.route("/play", methods=["GET", "POST"])
 def play():
-    if (request.method == "POST"):
+    if request.method == "POST":
         cmds = request.get_json()['data']
-        with sql.connect("database.db") as con:
-            cur = con.cursor()
-            # cur.execute('UPDATE start_robot SET data=1 WHERE rowid=1', (cmds,))
-            cur.execute('UPDATE commands_table SET data=? WHERE rowid=1', (cmds,))
-
+        start = request.get_json()['start']
+        cmdstr = start + cmds
         print(cmds)
-        print(type(cmds))
+        db.update_commands(cmdstr)
 
     return render_template("./play.html")
 
 
+@app.route("/reset", methods=["POST"])
+def reset():
+    reset = request.args.get('reset')
+    db.update_commands(reset)
+    return reset
+
+
 @app.route("/auth", methods=["POST"])
 def login():
-    now = time.time()
-    global btime
-    global ratelimit
-    global banned
-    if (banned and (now - btime) >= 10):
-        ratelimit = 0
-        banned = False
-
     user = request.form.get("login")
     password = request.form.get("password")
-    print(user)
-    print(password)
-    hpassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    if (user == ""):
-        return "Username cannot be empty"
-    if (password == ""):
-        return "Password cannot be empty"
-    if (user == uname and hpassword == passwd):
+    try:
+        a.login(user,password)
         flash("Successfully logged in")
         resp = make_response(render_template("./panel.html"))
         session["auth"] = "1"
         return resp
-    if (user != uname or hpassword != passwd):
-        if (ratelimit >= 5 and banned == False):
-            btime = time.time()
-            banned = True
-            return "5 invalid tries, account locked out"
-        ratelimit += 1
-        return "Invalid username or password"
+    except NameError as e:
+        return str(e)
 
 
-@app.route("/changespeed", methods=["POST"])
+@app.route("/adminsettings", methods=["GET"])
+def adminsettings():
+    return render_template("./adminsettings.html")
+
+app.config["MAPIMAGE_UPLOADS"] = "static/img/map/"
+app.config["MAPIMAGE_EXTENSIONS"] = ["jpeg", "jpg", "png"]
+
+def mapImage_ext(filename):
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.lower() in app.config["MAPIMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+@app.route("/changemaxspeed", methods=["GET","POST"])
 def cspeed():
-    pass
+
+    if (request.method == "GET"):
+
+        maps = os.listdir(app.config["MAPIMAGE_UPLOADS"])
+        mapspath = []
+
+        for map in maps:
+            mapspath += [os.path.join(app.config["MAPIMAGE_UPLOADS"], map)]
+        
+        print(mapspath)
+        return render_template("./adminsettings.html",maplist=mapspath)
+
+    if (request.method == "POST"):
+        
+        maxspeed = request.form.get("maxspeed")
+        db.update_data(maxspeed)
+
+    return render_template("./adminsettings.html")
+
+@app.route("/maxspeed", methods=["GET"])
+def maxspeed():
+    maxspeed = db.get_maxspeed()
+    return maxspeed
 
 
-# ???
+@app.route("/uploadmapimage", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
 
-@app.route("/changedist", methods=["POST"])
-def cdist():
-    pass
+        if request.files:
+            mapimage = request.files["mapimage"]
+            
+            if mapimage.filename == "":
+                print("No file selected")
+                return render_template("./adminsettings.html")
+                
+            if not mapImage_ext(mapimage.filename):
+                print("Required Extension: .jpeg, .jpg, .png")
+                return render_template("./adminsettings.html")
+                
+            else:
+                mapfilename = secure_filename(mapimage.filename) 
+            
+            path = os.path.join(app.config["MAPIMAGE_UPLOADS"], mapfilename)
 
+            mapimage.save(path)
 
-# ???
+            print("Image saved")
 
-@app.route("/changepass", methods=["GET", "POST"])
+            maps = os.listdir(app.config["MAPIMAGE_UPLOADS"])
+
+            mapspath = []
+            for map in maps:
+                mapspath += [os.path.join(app.config["MAPIMAGE_UPLOADS"], map)]
+
+            print(mapspath)
+            return render_template("./adminsettings.html",maplist=mapspath)
+            
+        return render_template("./adminsettings.html")
+    return render_template("./adminsettings.html")
+
+@app.route("/changepass", methods=["GET","POST"])
 def cpass():
     try:
         cookie = session["auth"]
     except:
         return render_template("./admin.html")
-    if (cookie == None):
+    if(cookie == None):
         return render_template("./admin.html")
-    if (request.method == "GET"):
-        return render_template("./changepass.html")
-    print(request.form)
-    global passwd
-    a = passwd
-    oldpass = request.form.get("oldpass")
-    holdpass = hashlib.sha256(oldpass.encode('utf-8')).hexdigest()
+    if(request.method == "GET"):
+        return render_template("./adminsettings.html")
     newpass = request.form.get("newpass")
     cnewpass = request.form.get("cfmnewpass")
-    if (oldpass == newpass):
-        return "Old password cannot be the same as new password"
-    if (oldpass == ""):
-        return "Old password cannot be empty"
-    if (newpass == ""):
-        return "New password cannot be empty"
-    if (cnewpass == ""):
-        return "Confirm new password cannot be empty"
-    if (holdpass != a):
-        return "Incorrect old password"
-    elif (newpass != cnewpass):
-        return "Passwords do not match"
-    elif (not (re.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$", newpass))):
-        return "Password should be 6-20 chars long with upper and lower case characters and special characters"
-    elif (holdpass == a):
-        hashed = hashlib.sha256(newpass.encode('utf-8')).hexdigest()
+    oldpass = request.form.get("oldpass")
+    hashed = hashlib.sha256(newpass.encode('utf-8')).hexdigest()
+    try:
+        a.change(oldpass,newpass,cnewpass,hashed)
         dotenv.set_key(dotenv_file, "PASS", hashed)
-        passwd = dotenv.dotenv_values(dotenv_file)["PASS"]
         return "password successfully changed"
+    except NameError as e:
+        return str(e)
 
 
 if __name__ == "__main__":
-    # app.run(host='192.168.43.217', port=80)
     app.run()
